2be0ef63fa84a209f92060c49de34dc7e1d3e8d5
Gerson Montenegro <montenegrogerson@gmail.com>
2018-11-21 20:54:18 +1300

    Creating objects to get basic products and cart reducers

--- src/reducers/ProductsReducer.js
+++ src/reducers/ProductsReducer.js
@@ -5,3 +5,20 @@ const initialState = {
     products,
     cartProducts
 };
+
+const productReducer = (state = initialState.products, action) => {
+    switch (action.type) {
+        default: {
+            return state;
+        }
+    }
+}
+
+const cartReducer = (state = initialState.cartProducts, action) => {
+    switch (action.type) {
+        case 'ADD_PRODUCT': {
+            return [...state, action.payload];
+        }
+    }
+    return state;
+}
