84d216b755c63bfb3739865ea2197eb51659a576
Gerson Montenegro <montenegrogerson@gmail.com>
2018-11-21 20:38:51 +1300

    Adding base file for the main list products interface

---
+++ src/screens/list/index.js
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react';
+import { View, FlatList } from 'react-native';
+import { Text } from 'react-native-elements';
+
+class ProductList extends PureComponent {
+    constructor(props) {
+        super(props);
+    }
+
+    onKeyExtractor = (item, index) => index.toString();
+
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', flexDirection: 'column', backgroundColor: 'gray' }}>
+                <Text>I'm the list!</Text>
+            </View>
+        );
+    }
+}
+
+export default ProductList;
