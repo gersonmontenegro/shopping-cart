b9c3f3f925b0758ccce4d1c7bb9e804ac6f5a72c
Gerson Montenegro <montenegrogerson@gmail.com>
2018-11-21 22:55:14 +1300

    Adding action to remove a product from the cart

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,5 +4,9 @@ export const mapDispatchToProps = (dispatch) => {
             type: 'ADD_PRODUCT',
             payload: product
         }),
+        removeProduct: (product) => dispatch({
+            type: 'REMOVE_PRODUCT',
+            payload: product
+        }),
     }
 }
