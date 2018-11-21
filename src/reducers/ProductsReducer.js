60001ecf9490cfd583a09ce782115a36c49cc4c7
Gerson Montenegro <montenegrogerson@gmail.com>
2018-11-21 22:57:11 +1300

    Implemeting the reducer to remove a producto from the cart. If there is no more quantities, remove the product

--- src/reducers/ProductsReducer.js
+++ src/reducers/ProductsReducer.js
@@ -29,6 +29,16 @@ const cartReducer = (state = initialState.cartProducts, action) => {
             }
             return [...state];
         }
+        case 'REMOVE_PRODUCT': {
+            state.forEach((product, index) => {
+                if (product.name == action.payload.name) {
+                    product.quantity--;
+                    if (product.quantity == 0) {
+                        state.splice(index, 1);
+                    }
+                }
+            })
+            return [...state];
         }
     }
     return state;
