9fd577044b018d257ef8cd2abef68fd068d685f4
Gerson Montenegro <montenegrogerson@gmail.com>
2018-11-21 22:15:33 +1300

    Adding styles file

---
+++ src/assets/styles/index.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const productStyles = StyleSheet.create({
+    title: {
+        left: 10,
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    price: {
+        left: 10,
+    },
+    icon: {
+        color: 'black',
+        backgroundColor: 'rgba(255,255,255,0)',
+        left: 5
+    },
+});
