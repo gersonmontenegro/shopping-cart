3b2851f27548725d876f4ccd2bec082a14cced58
Gerson Montenegro <montenegrogerson@gmail.com>
2018-11-21 20:43:54 +1300

    Implementing navigator instead call the list interface

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import ProductList from './screens/list';
+import { MainNavigator } from './navigation/';
 
 export default class App extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center" }}>
-        <ProductList />
+        <MainNavigator />
       </View>
     );
   }
